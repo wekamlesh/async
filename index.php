e37c01b3bbbe6a0f80adefbd76d3d8ddd0d7f90f
kamlesh merugu <merugukamlesh@gmail.com>
2020-10-18 01:51:11 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Main template File
+
+@package Async
+
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Async theme</title>
+</head>
+<body>
+    <p>Hello Wordpress</p>
+</body>
+</html>
